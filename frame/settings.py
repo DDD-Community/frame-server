cff82d6a60b715602bc8cdbfc4d1082bfa1f5178
sudosubin <sudosubin+github@gmail.com>
2020-05-27 21:38:27 +0900

    Add allowed hosts

--- frame/settings.py
+++ frame/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'frame-server.herokuapp.com'
+]
 
 
 # Application definition
